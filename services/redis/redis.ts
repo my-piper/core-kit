1f763b3b708edc43ff6bcb00cb96f0d1170d6086
breslavsky <breslavsky.anton@gmail.com>
2025-03-25 00:15:15 +0100

    fix: return error for redis

--- services/redis/redis.ts
+++ services/redis/redis.ts
@@ -1,5 +1,20 @@
 import { createClient } from "@redis/client";
+import { createLogger } from "../logger";
 import { REDIS_PASSWORD, REDIS_URL } from "./consts";
 
+const logger = createLogger("redis");
+
 export const redis = createClient({ url: REDIS_URL, password: REDIS_PASSWORD });
+logger.error(`Connect to redis ${REDIS_URL}`);
 await redis.connect();
+
+redis.on("error", (err: { code?: string; message: string }) => {
+  logger.error(`Redis error: ${err.message}`);
+  if (
+    err.code === "ECONNREFUSED" ||
+    err.code === "EHOSTUNREACH" ||
+    err.code === "ENOTFOUND"
+  ) {
+    process.kill(process.pid, "SIGINT");
+  }
+});
