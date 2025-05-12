e83f985449639134d8478955356397a354afff7e
breslavsky <breslavsky.anton@gmail.com>
2025-05-12 13:30:32 +0200

    fix: internal imports

--- packages/transform/index.ts
+++ packages/transform/index.ts
@@ -1,3 +1,15 @@
 export * from "class-transformer";
+export {
+  IsEmail,
+  IsEnum,
+  IsNotEmpty,
+  IsOptional,
+  IsUrl,
+  Length,
+  Matches,
+  MaxLength,
+  MinLength,
+  ValidateNested,
+} from "class-validator";
 export * from "./primitive";
 export * from "./utils";
