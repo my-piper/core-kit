a0e3c0ce7ef3d8b0f172b1e39ed58d414e1570bb
breslavsky <breslavsky.anton@gmail.com>
2025-08-04 13:33:18 +0200

    feat: add json model transformers

--- packages/transform/object.ts
+++ packages/transform/object.ts
@@ -1,15 +1,18 @@
 import { TransformationType } from "class-transformer";
+import { toInstance, toPlain } from "core-kit/packages/transform/utils";
 
 export function objectTransformer({
   value,
   type,
 }: {
-  value: object;
+  value: object | string;
   type: TransformationType;
 }) {
   switch (type) {
     case TransformationType.PLAIN_TO_CLASS: {
-      return value as object;
+      return typeof value === "string"
+        ? JSON.parse(value as string)
+        : (value as object);
     }
     case TransformationType.CLASS_TO_PLAIN: {
       return value as object;
@@ -61,3 +64,61 @@ export const objectMapTransformer =
         throw new Error("It is not supported");
     }
   };
+export const jsonModelTransformer =
+  <T>(model: new () => T) =>
+  <T>({
+    value,
+    type,
+  }: {
+    value: string | object | T;
+    type: TransformationType;
+  }) => {
+    switch (type) {
+      case TransformationType.PLAIN_TO_CLASS: {
+        if (value === undefined) {
+          return undefined;
+        }
+        if (value === null) {
+          return null;
+        }
+        const obj =
+          typeof value === "string"
+            ? JSON.parse(value as string)
+            : (value as object);
+        return toInstance(obj, model);
+      }
+      case TransformationType.CLASS_TO_PLAIN: {
+        if (value === undefined) {
+          return undefined;
+        }
+        if (value === null) {
+          return null;
+        }
+        const source = value as T;
+        return JSON.stringify(toPlain(source));
+      }
+      default:
+        throw new Error("It is not supported");
+    }
+  };
+
+export function jsonObjectTransformer({
+  value,
+  type,
+}: {
+  value: string | object;
+  type: TransformationType;
+}) {
+  switch (type) {
+    case TransformationType.PLAIN_TO_CLASS: {
+      return typeof value === "string"
+        ? JSON.parse(value as string)
+        : (value as object);
+    }
+    case TransformationType.CLASS_TO_PLAIN: {
+      return JSON.stringify(value as object);
+    }
+    default:
+      throw new Error("It is not supported");
+  }
+}
